3340dbed4ed4ed453d2cbd02b1b9be8fc6d45646
paraglande03 <paraglande03@gmail.com>
2021-03-12 23:25:25 +0530

    Added: test cses to check enhanced invoice

--- src/test/java/InvoiceServiceTest.java
+++ src/test/java/InvoiceServiceTest.java
@@ -28,12 +28,13 @@ public class InvoiceServiceTest {
     }
 
     @Test
-    public void givenMultipleRides_shouldReturnTotalFare() {
+    public void givenMultipleRides_shouldReturnInvoiceSummary() {
         
         Ride[] rides= {new Ride(2.0, 5),
                        new Ride(0.1, 1)
                       };
-        double fare=invoiceGenerator.calculateFare(rides);
-        Assert.assertEquals(30,fare,0.0);
+        InvoiceSummary summary =invoiceGenerator.calculateFare(rides);
+       InvoiceSummary expectedInvoiceSummary= new InvoiceSummary(2, 30.0);
+        Assert.assertEquals(expectedInvoiceSummary,summary);
     }
 }
