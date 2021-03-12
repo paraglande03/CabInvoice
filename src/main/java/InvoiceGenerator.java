2814db1f801314d3dc14f56fcbb3cc1c307ed9c2
paraglande03 <paraglande03@gmail.com>
2021-03-12 23:00:38 +0530

    Added: Test case for Multiple fares

--- src/main/java/InvoiceGenerator.java
+++ src/main/java/InvoiceGenerator.java
@@ -10,4 +10,13 @@ public class InvoiceGenerator {
             else
                 return totalFare;
     }
+
+    public double calculateFare(Ride[] rides) {
+        double totalFare=0;
+        for (Ride ride:rides){
+            totalFare+=this.calculateFare(ride.distance, ride.time);
+
+        }
+        return totalFare;
+    }
 }
--- src/test/java/InvoiceServiceTest.java
+++ src/test/java/InvoiceServiceTest.java
@@ -19,4 +19,14 @@ public class InvoiceServiceTest {
         double fare= invoiceGenerator.calculateFare(distance,time);
         Assert.assertEquals(5,fare,0.0);
     }
+
+    @Test
+    public void givenMultipleRides_shouldReturnTotalFare() {
+        InvoiceGenerator invoiceGenerator=new InvoiceGenerator();
+        Ride[] rides= {new Ride(2.0, 5),
+                       new Ride(0.1, 1)
+                      };
+        double fare=invoiceGenerator.calculateFare(rides);
+        Assert.assertEquals(30,fare,0.0);
+    }
 }
