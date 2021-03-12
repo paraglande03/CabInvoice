7f2ce9f968f24b8515e481e8c3c117c17f6e5761
paraglande03 <paraglande03@gmail.com>
2021-03-12 22:40:02 +0530

    Added: Test case for Minimum fare

--- src/main/java/InvoiceGenerator.java
+++ src/main/java/InvoiceGenerator.java
@@ -1,8 +1,13 @@
 public class InvoiceGenerator {
     private static final double MINIUM_COST_PER_KILOMETER = 10.0;
     private static final int COST_PER_TIME =1 ;
+    private static final double MINIUM_FARE = 5;
 
     public double calculateFare(double distance, int time) {
-        return distance*MINIUM_COST_PER_KILOMETER+time*COST_PER_TIME;
+        double totalFare= distance*MINIUM_COST_PER_KILOMETER+time*COST_PER_TIME;
+        if(totalFare<MINIUM_FARE)
+            return MINIUM_FARE;
+            else
+                return totalFare;
     }
 }
